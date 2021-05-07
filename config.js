fd7089036a1a3ae194c3756d9a1ca4d3dfc2ad2e
Joaquin Touron <43842304+JayexDesigns@users.noreply.github.com>
2021-05-07 21:12:51 +0200

    improved default configuration

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 var showFPS = false;
 
-var particleColor = "#ffffff";
-var lineColor = "#ffffff";
-
 var transparentBackground = false;
 var backgroundColor1 = "#7b00ff";
 var backgroundColor2 = "#1a237e";
 
-var particleQuantity = 100;
+var particleColor = "#ffffff";
+var lineColor = "#ffffff";
 
-var minVelocity = 0.1;
-var maxVelocity = 1.5;
+var particleQuantity = 100;
 
 var particleRadius = 3;
 var lineWidth = 2;
+
+var minVelocity = 0.5;
+var maxVelocity = 2;
